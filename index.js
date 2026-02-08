669d296661e2e347a3a9040ae185f15f40c44d04
shoybit <shoyaib105@gmail.com>
2026-02-08 20:25:37 +0600

    GraphQL if blocked

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import app from "./app.js";
-// import { connectDB } from "./src/config/db.js";
-// import "./src/jobs/github.cron.js";
-
-// const PORT = process.env.PORT || 5000;
-
-// connectDB();
-
-
-
-// export default app;
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -22,12 +7,8 @@ import "./src/jobs/github.cron.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("DB connection failed:", err);
-  });
+connectDB();
+
+
+
+export default app;
